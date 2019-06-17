a4eeb40c50bec256b1edaf5891a991e38c1d5793
matheus-lopes <matheus.lopes@soaexpert.com.br>
2019-06-17 17:25:31 -0300

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  API_GATEWAY:  
+  // 'http://localhost:3000'
+  'https://pergunteme-api.azurewebsites.net/api',
+
+  API_NGROK: 'https://a245fe72.ngrok.io/'
 };
